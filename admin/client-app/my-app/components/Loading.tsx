197daf4fef7da2a18c47dd6c033eddc120206c39
hiteshmeshram <hiteshmeshramt@gmail.com>
2024-10-03 07:36:23 +0530

    added pagination

--- admin/client-app/my-app/components/Loading.tsx
+++ admin/client-app/my-app/components/Loading.tsx
@@ -1,7 +1,16 @@
+"use client"
+import React, { useState } from 'react';
+import Loader from "react-js-loader";
+import LoadingBar from 'react-top-loading-bar'
 export const Loading = ()=>{
+    const [progress,setProgress] = useState(50)
     return(
-        <div>
-            loading
-        </div>
+        // <div className="item w-full">
+        //             <Loader type="rectangular-ping" bgColor="gray" color="purple"  size={1000} />
+        // </div>
+    <div className='min-h-1'>
+        <LoadingBar color="red" progress={progress}
+    onLoaderFinished={() => setProgress(0)} />
+    </div>
     )
 }
